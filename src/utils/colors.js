95037f7ef9329655563a6fb886e23951b39e7275
Antony Howlett <hello@anthowlett.co.uk>
2019-02-06 20:42:09 +0000

    Set up utils

---
+++ src/utils/colors.js
@@ -0,0 +1,30 @@
+const colors = {
+    blue:       '#0C479D', // ExxonMobil Blue
+    dark_blue:  '#002f6c', // ExxonMobil Dark Blue
+    light_blue: '#00a1dd', // ExxonMobil Light Blue
+    red:        '#FE000C', // ExxonMobil Red
+    pink:       '#dc3672', // ExxonMobil Pink
+    maroon:     '#8d0858', // ExxonMobil Maroon
+    grey:       '#5A5A5A', // ExxonMobil Grey
+    light_grey: '#E3E3E3',
+    med_grey:   '#BDC3C7',
+    silver:     '#B5B5B5', // ExxonMobil Silver
+    off_black:  '#2B2926', // ExxonMobil Black
+
+    green:      '#00A14D', // ExxonMobil Green
+    orange:     '#ED8B00', // ExxonMobil Orange
+    amber:      '#F2AC33', // ExxonMobil Amber
+    yellow:     '#FFD700', // ExxonMobil Yellow
+
+    black_5:    '#F2F2F2', // ExxonMobil Black 5% tint
+    black_10:   '#E6E6E6', // ExxonMobil Black 10% tint
+    black_15:   '#D9D9D9', // ExxonMobil Black 15% tint
+    black_20:   '#CCCCCC', // ExxonMobil Black 20% tint
+    black_35:   '#A6A6A6', // ExxonMobil Black 35% tint
+    blue_70:    '#4C6DBF', // ExxonMobil Blue 70% tint
+
+    black:      '#000',
+    white:      '#fff'
+}
+
+export default colors;
