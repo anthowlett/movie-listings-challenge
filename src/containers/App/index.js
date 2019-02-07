5f22e1563d139f0504b5fd82036f08ea21192acc
Antony Howlett <hello@anthowlett.co.uk>
2019-02-07 10:56:54 +0000

    Create pre-loader component

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,17 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import styled from 'styled-components';
 import { loadData } from '../../actions';
 import Filters from '../../components/Filters';
+import Loader from '../../components/Loader';
 import NowPlayingMovies from '../../components/NowPlayingMovies';
-
-const Loading = styled.p`
-  padding: 50px;
-  text-align: center;
-  font-size: 20px;
-  font-weight: 400;
-  width: 100%;
-`;
 
 class App extends Component {
   componentDidMount() {
@@ -21,7 +13,7 @@ class App extends Component {
 
   render() {
     if (!this.props.loaded) {
-      return <Loading>Just a little longer...</Loading>
+      return <Loader />
     }
 
     return (
